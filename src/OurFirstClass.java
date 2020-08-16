02c8129576bd4f59b2cbcbd730e2d17f6a1ccead
David <59180074+david00180@users.noreply.github.com>
2020-08-16 15:42:00 +0300

    Initial commit

---
+++ src/OurFirstClass.java
@@ -0,0 +1,13 @@
+
+public class OurFirstClass
+	{
+public static void main(String[] args)
+	{
+		System.out.println("HELLO WORLD!");
+		int x=7;
+		System.out.println(x);
+		int y=5;
+		x=5;
+		System.out.println(x+y);
+	}
+	}
